2b4bcfd1ad5580e45c227ccdea08460d88671548
Maurilio Lucena <maurilio.lucena@ajaxti.com.br>
2022-05-17 17:03:49 -0300

    accordion

--- scripts.js
+++ scripts.js
@@ -1,20 +1,45 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('active');
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('active');
 
-    function activeTab(index) {
-        tabContent.forEach((item) => {
-            item.classList.remove('active');
+        const activeTab = (index) => {
+            tabContent.forEach((item) => {
+                item.classList.remove('active');
+            });
+
+            tabContent[index].classList.add('active');
+        }
+
+        tabMenu.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-
-        tabContent[index].classList.add('active');
     }
-
-    tabMenu.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            activeTab(index);
-        });
-    });
 }
+
+initTabNav();
+
+function initAccordion() {
+    if (accordionList.length) {
+        const activeClass = 'active';
+
+        const accordionList = document.querySelectorAll('.js-accordion dt');
+        accordionList[0].classList.add(activeClass);
+        accordionList[0].nextElementSibling.classList.add(activeClass);
+
+        function activeAccordion() {
+            this.classList.toggle(activeClass);
+            this.nextElementSibling.classList.toggle(activeClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();
