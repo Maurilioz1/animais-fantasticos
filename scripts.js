d6dcccf597ec21dea3f6b3a7ebc948e2fd652193
Maurilio Lucena <maurilio.lucena@ajaxti.com.br>
2022-05-18 10:53:18 -0300

    scroll animation

--- scripts.js
+++ scripts.js
@@ -68,7 +68,32 @@ function initScrollSmooth() {
     }
 }
 
+function initAnimationScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if (sections.length) {
+        const windowHalf = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            sections.forEach((item) => {
+                const sectionTop = item.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowHalf) < 0;
+
+                if (isSectionVisible) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active');
+                }
+            });
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
 
 initTabNav();
 initAccordion();
 initScrollSmooth();
+initAnimationScroll();
