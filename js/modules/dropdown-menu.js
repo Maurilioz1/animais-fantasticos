26328eea487323c032722e338f63f359b20ef7db
Maurilio Lucena <maurilio.lucena@ajaxti.com.br>
2022-05-25 14:23:05 -0300

    dropdown menu

---
+++ js/modules/dropdown-menu.js
@@ -0,0 +1,20 @@
+import outsideClick from "./outside-click.js";
+
+export default function initDropdownMenu() {
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+    function handleClick(event) {
+        event.preventDefault();
+
+        this.classList.add('active');
+        outsideClick(this, ['touchstart', 'click'], () => {
+            this.classList.remove('active');
+        });
+    }
+
+    dropdownMenus.forEach((item) => {
+        ['touchstart', 'click'].forEach((userEvent) => {
+            item.addEventListener(userEvent, handleClick);
+        });
+    });
+}
