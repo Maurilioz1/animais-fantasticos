1e439ad5d0b3abea1f3254dff1a616ec3218252d
Maurilio Lucena <maurilio.lucena@ajaxti.com.br>
2022-05-25 10:37:04 -0300

    tooltip

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@ import initScrollSmooth from './modules/scroll-smooth.js';
 import initAccordion from './modules/accordion.js';
 import initTabNave from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initAnimationScroll();
 initScrollSmooth();
 initAccordion();
 initTabNave();
 initModal();
+initTooltip();
